0726b05897819dfef2bb5976781b68651658d548
juechen-zzz <240553516@qq.com>
2020-12-07 16:19:09 +0800

    add Ajax in 2020-12-07

---
+++ SpringMVC/out/artifacts/springmvc_07_ajax_war_exploded/test2.jsp
@@ -0,0 +1,43 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+
+    <script src="${pageContext.request.contextPath}/statics/js/jquery-3.5.1.js"></script>
+    <script>
+        $(function () {
+            $("#btn").click(function () {
+                // $.post(url, param, success)
+                $.post("${pageContext.request.contextPath}/a2", function (data) {
+                    // console.log(data);
+                    var html = "";
+                    for (let i = 0; i < data.length; i++) {
+                        html += "<tr>" +
+                                "<td>" + data[i].name + "</td>" +
+                                "<td>" + data[i].age + "</td>" +
+                                "<td>" + data[i].sex + "</td>" +
+                                "</tr>"
+                    }
+                    $("#content").html(html);
+                });
+            })
+        });
+    </script>
+
+    </head>
+<body>
+
+<input type="button" value="加载数据" id="btn">
+<table>
+    <tr>
+        <td>姓名</td>
+        <td>年龄</td>
+        <td>性别</td>
+    </tr>
+    <tbody id="content">
+        <%--数据：后台--%>
+    </tbody>
+</table>
+
+</body>
+</html>
---
+++ SpringMVC/springmvc-07-ajax/src/main/java/com/komorebi/controller/AjaxController.java
@@ -0,0 +1,56 @@
+package com.komorebi.controller;
+
+import com.komorebi.pojo.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class AjaxController {
+    @RequestMapping("/t1")
+    public String test1(){
+        return "hello";
+    }
+
+    @RequestMapping("/a1")
+    public void ajax1(String name, HttpServletResponse response) throws IOException {
+        if ("admin".equals(name)){
+            response.getWriter().print("true");
+        }else{
+            response.getWriter().print("false");
+        }
+    }
+
+    @RequestMapping("/a2")
+    public List<User> a2(){
+        List<User> userList = new ArrayList<User>();
+        userList.add(new User("k1", 18, "man"));
+        userList.add(new User("k2", 18, "man"));
+        userList.add(new User("k3", 18, "man"));
+        return userList;
+    }
+
+    @RequestMapping("/a3")
+    public String a3(String name, String pwd){
+        String msg = "";
+        if (name != null){
+            if ("admin".equals(name)){
+                msg = "OK";
+            }else {
+                msg = "用户名输入错误";
+            }
+        }
+        if (pwd != null){
+            if ("123456".equals(pwd)){
+                msg = "OK";
+            }else {
+                msg = "密码输入有误";
+            }
+        }
+        return msg; //由于@RestController注解，将msg转成json格式返回
+    }
+}
