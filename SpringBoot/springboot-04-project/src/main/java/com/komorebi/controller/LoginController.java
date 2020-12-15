ef83b396605b1ebdc9b5f1495712499c37323241
juechen-zzz <240553516@qq.com>
2020-12-15 22:22:22 +0800

    add in 2020-12-15

---
+++ SpringBoot/springboot-04-project/src/main/java/com/komorebi/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.komorebi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+    @RequestMapping("/user/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password, Model model){
+        // 具体业务，判定用户密码是否正确
+        if ("admin".equals(username) &&"123456".equals(password)){
+            return "redirect:/main.html";
+        }
+        else {
+            // 告诉用户失败了
+            model.addAttribute("msg", "用户名或密码错误");
+            return "index";
+        }
+    }
+}
