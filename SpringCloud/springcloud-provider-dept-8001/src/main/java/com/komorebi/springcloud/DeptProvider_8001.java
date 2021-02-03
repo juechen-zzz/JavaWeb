76138e0daf54d1add50eadd279439292cdbafb9b
juechen-zzz <240553516@qq.com>
2021-02-03 23:21:24 +0800

    add in 2020-2-3

--- SpringCloud/springcloud-provider-dept-8001/src/main/java/com/komorebi/springcloud/DeptProvider_8001.java
+++ SpringCloud/springcloud-provider-dept-8001/src/main/java/com/komorebi/springcloud/DeptProvider_8001.java
@@ -2,9 +2,13 @@ package com.komorebi.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 // 启动类
 @SpringBootApplication
+@EnableEurekaClient         // 在服务启动后，自动注册到Eureka中
+@EnableDiscoveryClient      // 服务发现
 public class DeptProvider_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_8001.class, args);
--- SpringCloud/springcloud-provider-dept-8001/src/main/java/com/komorebi/springcloud/controller/DeptController.java
+++ SpringCloud/springcloud-provider-dept-8001/src/main/java/com/komorebi/springcloud/controller/DeptController.java
@@ -3,10 +3,9 @@ package com.komorebi.springcloud.controller;
 import com.komorebi.springcloud.pojo.Dept;
 import com.komorebi.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +14,9 @@ import java.util.List;
 public class DeptController {
     @Autowired
     private DeptService deptService;
+
+    @Autowired
+    private DiscoveryClient client;
 
     @PostMapping("/dept/add")
     public boolean addDept(Dept dept){
@@ -30,4 +32,25 @@ public class DeptController {
     public List<Dept> queryAll(){
         return deptService.queryAll();
     }
+
+    // 注册进来的微服务，获取一些信息
+    @GetMapping("/dept/discovery")
+    public Object discovery(){
+        // 获取微服务列表的清单
+        List<String> services = client.getServices();
+        System.out.println("discovery --> " + services);
+
+        // 得到一个具体的微服务信息
+        List<ServiceInstance> instances = client.getInstances("SPRINGCLOUD-PROVIDER-DEPT");
+        for (ServiceInstance instance : instances) {
+            System.out.println(
+                    instance.getHost() + "\t" +
+                    instance.getPort() + "\t" +
+                    instance.getUri() + "\t" +
+                    instance.getServiceId()
+            );
+        }
+        return this.client;
+    }
+
 }
