0b368b17f9ec574d71ff20e0669b2ba8a95dde94
juechen-zzz <240553516@qq.com>
2021-02-07 19:23:30 +0800

    finish springcloud

---
+++ SpringCloud/springcloud-config-client-8201/src/main/java/com/komorebi/springcloud/ConfigClient_8201.java
@@ -0,0 +1,11 @@
+package com.komorebi.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClient_8201 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_8201.class, args);
+    }
+}
---
+++ SpringCloud/springcloud-config-client-8201/src/main/java/com/komorebi/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.komorebi.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+    @Value("${spring.application.name}")
+    private String applicationName;
+    @Value("${eureka.client.service-url.defaultZone}")
+    private String eurekaServer;
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/config")
+    public String getConfig() {
+        return "applicationName: " + applicationName + "\t" +
+                "eurekaServer: " + eurekaServer + "\t" +
+                "port: " + port;
+    }
+}
